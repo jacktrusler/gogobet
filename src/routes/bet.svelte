ba62aa0e8f326e22cd2d95c2345dab448cea1ad5
Jack <jtrus93@gmail.com>
2022-03-14 16:12:14 -0400

    Rawr

---
+++ src/routes/bet.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<div class="flex flex-col justify-center items-center h-screen bet-screen">
+  <input class="w-100 bet-box h-9 p-3 m-10" placeholder="Invite Friends" />
+  <input class="w-100 bet-box h-9 p-3" placeholder="Place Your Bet" />
+</div>
+
+<style>
+  .bet-screen{
+    background: linear-gradient(
+		180deg,
+		#a94604,
+		#d56114 20%,
+		#f97015
+	);
+  }
+
+  .bet-box{
+    border-radius: 10px;
+  }
+</style>
