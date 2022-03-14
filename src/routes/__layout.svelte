54bb3abee611d5f1f64d32d29655683d07d47803
Jack <jtrus93@gmail.com>
2022-03-14 16:07:25 -0400

    O.o'

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,15 +9,6 @@
 </main>
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: none;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+	
 
 </style>
